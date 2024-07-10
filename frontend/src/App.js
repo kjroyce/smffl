7f49f84a1b4a0f95d8bacdc297b9fae850691abf
Royce Joseph <roycekj@ad.unc.edu>
2024-07-10 16:35:17 -0400

    Set up frontend using react framework and backend with flask framework

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get('/api/data')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {data ? <p>{data.message}</p> : <p>Loading...</p>}
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
